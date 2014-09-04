4fc09c4128cf1998bea33f519230fe16b699a521
piotrooo <piotroo89@gmail.com>
2014-09-04 22:38:27 +0200

    Call static finders.

--- src/SugarClient/Module.php
+++ src/SugarClient/Module.php
@@ -1,9 +1,19 @@
 <?php
 namespace SugarClient;
 
+use SugarClient\Finder\DynamicFinder;
+
 abstract class Module
 {
-    public function __call($name, $arguments)
+    public static function findById($id)
     {
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        $dynamicFinder = DynamicFinder::match($name);
+        if ($dynamicFinder) {
+
+        }
     }
 }
